081106b9f766dc8749a6b197c7cc1b2cbd581298
MelissaAlasalmi <melissa.alasalmi@gmail.com>
2022-11-04 23:44:10 +0200

    frontend boilerplate

---
+++ frontend/src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './styles/global.css';
+
+const App: React.FC = () => {
+  return (
+    <div>
+      <p className='text-sm font-medium text-orange-200'>Hello, yall</p>
+    </div>
+  );
+};
+
+export default App;
