09207c133bd53816a75f09d79e40d2705cc5aa7c
MelissaAlasalmi <melissa.alasalmi@gmail.com>
2022-11-05 10:14:41 +0200

    added todo list

--- frontend/src/components/todoItem.tsx
+++ frontend/src/components/todoItem.tsx
@@ -7,7 +7,22 @@ type Props = {
 
 const TodoItem = (props: Props) => {
   const { item } = props;
-  return <li>{item.title}</li>;
+  return (
+    <div className="flex items-start items-center mb-2.5 px-5">
+      <input
+        id="checkbox-2"
+        aria-describedby="checkbox-2"
+        type="checkbox"
+        className="w-4 h-4 border-gray-300 rounded bg-gray-50 focus:ring-3 focus:ring-blue-300"
+      />
+      <label
+        htmlFor="checkbox-2"
+        className="ml-3 text-lg font-medium text-gray-900"
+      >
+        {item.title}
+      </label>
+    </div>
+  );
 };
 
 export default TodoItem;
--- frontend/src/components/todoList.tsx
+++ frontend/src/components/todoList.tsx
@@ -9,11 +9,28 @@ type Props = {
 const TodoList = (props: Props) => {
   const { list } = props;
   return (
-    <ul>
-      {list.map((i) => (
-        <TodoItem key="i.title" item={i} />
-      ))}
-    </ul>
+    <div>
+      <div className="max-w-2xl mx-auto">
+        <div className="max-w-sm bg-white border border-gray-200 rounded-lg shadow-md">
+          <h1 className="pt-5 pr-5 mb-2 text-2xl font-bold tracking-tight text-blue-800 pl-9">
+            What would you like to get done today?
+          </h1>
+          <div className="px-5">
+            <div className="container">
+              <div className="flex flex-col grid-cols-9 p-2 mx-auto md:grid text-blue-50"></div>
+              <div className="max-w-lg mx-auto">
+                <fieldset className="mb-5">
+                  <legend className="sr-only">Checkbox variants</legend>
+                  {list.map((i) => (
+                    <TodoItem key="i.title" item={i} />
+                  ))}
+                </fieldset>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
   );
 };
 
