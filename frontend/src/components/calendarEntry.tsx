c51050e68dfb7568d2b620e2ee17cb3b41cbd5f5
MelissaAlasalmi <melissa.alasalmi@gmail.com>
2022-11-05 09:47:36 +0200

    changed the styles a bit

--- frontend/src/components/calendarEntry.tsx
+++ frontend/src/components/calendarEntry.tsx
@@ -9,13 +9,13 @@ const CalEntry = (props: Props) => {
   const { item } = props;
   return (
     <div className="flex md:contents">
-      <div className="relative col-start-5 col-end-6 mr-10 md:mx-auto">
-        <div className="flex items-center justify-center w-6 h-full">
+      <div className="relative md:mx-auto">
+        <div className="flex items-center justify-center w-6 h-full ">
           <div className="w-1 h-full bg-blue-800 pointer-events-none"></div>
         </div>
         <div className="absolute w-6 h-6 -mt-3 bg-blue-500 rounded-full shadow top-1/2"></div>
       </div>
-      <div className="col-start-6 col-end-10 p-4 my-4 mr-auto bg-blue-500 shadow-md rounded-xl">
+      <div className="col-start-2 col-end-10 p-4 my-4 mr-auto bg-blue-500 border shadow-md rounded-xl">
         <h3 className="mb-1 text-lg font-semibold">{item.title}</h3>
         <p className="leading-tight text-justify">Get it done, boothang!</p>
       </div>
