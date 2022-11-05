7541780a2e44f0235c09ef7a682be4f75083f393
MelissaAlasalmi <melissa.alasalmi@gmail.com>
2022-11-05 10:00:20 +0200

    added style to cal view

--- frontend/src/components/calendar.tsx
+++ frontend/src/components/calendar.tsx
@@ -12,13 +12,20 @@ const Calendar = (props: Props) => {
 
   return (
     <div>
-      <h1 className="mb-1 text-lg font-semibold">{title}</h1>
-      <p>Cook some yummers food</p>
-      <div className="container">
-        <div className="flex flex-col grid-cols-9 p-2 mx-auto md:grid text-blue-50">
-          {list.map((i) => (
-            <CalEntry key="i.title" item={i} />
-          ))}
+      <div className="max-w-2xl mx-auto">
+        <div className="max-w-sm bg-white border border-gray-200 rounded-lg shadow-md">
+          <h1 className="pt-5 mb-2 text-2xl font-bold tracking-tight text-blue-800 pl-9">
+            {title}
+          </h1>
+          <div className="px-5">
+            <div className="container">
+              <div className="flex flex-col grid-cols-9 p-2 mx-auto md:grid text-blue-50">
+                {list.map((i) => (
+                  <CalEntry key="i.title" item={i} />
+                ))}
+              </div>
+            </div>
+          </div>
         </div>
       </div>
     </div>
